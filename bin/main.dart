5121c4c7a7c2f1626e7aa2af53c8aa96e104d314
yzh <48515824@qq.com>
2019-10-10 15:00:50 +0800

    'init'

---
+++ bin/main.dart
@@ -0,0 +1,18 @@
+import 'package:dartLib/dartLib.dart' as dartLib;
+
+main(List<String> arguments) {
+  print('Hello world: ${dartLib.calculate()}!');
+
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects= ['Jupiter','Saturn','Uranus','Neptune'];
+  var image = {
+    'tags':['saturn'],
+    'url':'//path/to/saturn.jpg'
+  };
+
+
+
+
+}
